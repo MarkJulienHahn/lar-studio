f7de7ed250c7aeec778f14a65e4f4a9297444bf8
Mark Julien Hahn <mail@markjulienhahn.de>
2025-07-03 12:23:46 +0200

    Newsletter

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -1,34 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function MyForm() {
   const [email, setEmail] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const formRef = useRef(null);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission here, e.g., send data to the server
-    setSubmitted(true);
-
-    // You might want to send email to the server using fetch or Axios
-    // Example:
-    // fetch('your_api_endpoint', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ email }),
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //   // Handle response
-    // })
-    // .catch(error => {
-    //   // Handle error
-    // });
+    // NICHT preventDefault verwenden! Wir wollen das Formular ja abschicken.
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 1000); // kurze Verzögerung, damit das Formular abgesendet werden kann
   };
 
   return (
@@ -39,21 +24,26 @@ function MyForm() {
       <div className="line"></div>
       <div className="newsletterText">
         <p>
-          Wir versenden ca. 4-6 Newsletter im Jahr. Je nachdem was aktuell
-          ansteht und uns bewegt variieren die Inhalte zwischen Beiträgen zu
-          neuen Projekten oder auch Einladungen zu kleinen Veranstaltungen wie
+          Wir versenden ca. 4–6 Newsletter im Jahr. Je nachdem, was aktuell
+          ansteht und uns bewegt, variieren die Inhalte zwischen Beiträgen zu
+          neuen Projekten oder Einladungen zu kleinen Veranstaltungen wie
           Vernissagen und Tastings. <br />
           <br />
           Wenn Sie ebenfalls unseren Newsletter erhalten möchten, tragen Sie
-          sich gerne nachstehend ein. Ihre Daten werden nicht weiteregegeben
+          sich gerne nachstehend ein. Ihre Daten werden nicht weitergegeben
           oder anderweitig verwendet.
         </p>
       </div>
+
+      <iframe name="hidden_iframe" style={{ display: "none" }} />
+
       {!submitted ? (
         <div className="formWrapper">
           <form
+            ref={formRef}
             method="post"
             action="https://td1d27ab7.emailsys1a.net/120/2299/fb52a5ad0d/subscribe/form.html?_g=1700133690"
+            target="hidden_iframe"
             className="rmBase__content"
             onSubmit={handleSubmit}
           >
@@ -66,13 +56,13 @@ function MyForm() {
               onChange={handleEmailChange}
               required
             />
-
             <button type="submit">ABSCHICKEN</button>
           </form>
         </div>
       ) : (
         <p className="confirmation">
-          Sie haben sich erfolgreich mit der Mailadresse: {email} angemeldet.
+          Sie haben einen Bestätigungslink an die Mailadresse{" "}
+          <strong>{email}</strong> bekommen.
         </p>
       )}
     </div>
@@ -80,74 +70,3 @@ function MyForm() {
 }
 
 export default MyForm;
-
-// import { useEffect } from 'react';
-
-// const NewsletterForm = () => {
-//   useEffect(() => {
-//     const script = document.createElement('script');
-//     script.src =
-//       'https://td1d27ab7.emailsys1a.net/form/120/2739/28c1c1f69a/embedded.js';
-//     script.async = true;
-//     document.body.appendChild(script);
-
-//     return () => {
-//       document.body.removeChild(script);
-//     };
-//   }, []);
-
-//   return (
-//     <div id="rmOrganism">
-//       <div className="rmEmbed rmLayout--vertical rmBase">
-//         <div
-//           data-page-type="formSubscribe"
-//           className="rmBase__body rmSubscription"
-//         >
-//           <form
-//             method="post"
-//             action="https://td1d27ab7.emailsys1a.net/120/2299/fb52a5ad0d/subscribe/form.html?_g=1700133690"
-//             className="rmBase__content"
-//           >
-//             <div className="rmBase__container">
-//               <div className="rmBase__section">
-//                 <div
-//                   className="rmBase__el rmBase__el--input rmBase__el--label-pos-left"
-//                   data-field="email"
-//                 >
-//                   <label
-//                     htmlFor="email"
-//                     className="rmBase__compLabel rmBase__compLabel--hideable"
-//                   >
-//                     E-Mail
-//                   </label>
-//                   <div className="rmBase__compContainer">
-//                     <input
-//                       type="text"
-//                       name="email"
-//                       id="email"
-//                       placeholder=""
-//                       className="rmBase__comp--input comp__input"
-//                     />
-//                     <div className="rmBase__compError"></div>
-//                   </div>
-//                 </div>
-//               </div>
-
-//               <div className="rmBase__section">
-//                 <div className="rmBase__el rmBase__el--cta">
-//                   <button type="submit" className="rmBase__comp--cta">
-//                     Anmelden
-//                   </button>
-//                 </div>
-//               </div>
-
-//             </div>
-//           </form>
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NewsletterForm;
